4ae30322992610388cc261f02248c1e0eb93903b
Gabriel Matos <gab.matos@outlook.com>
2018-12-13 16:41:41 +0000

    Created and displayed user input vs one another

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   get '/' do
     "Hello Battle!"
+    erb(:index)
+  end
+
+  post '/names' do
+    @name1 = params[:Player1_name]
+    @name2 = params[:Player2_name]
+    erb(:play)
   end
   run! if app_file == $0
 end
--- spec/testing_infrastructure_spec.rb
+++ spec/testing_infrastructure_spec.rb
@@ -1,7 +1,17 @@
 
-feature 'User visits website' do
-  scenario'Testing infrastructure' do
+# feature 'User visits website' do
+#   scenario'Testing infrastructure' do
+#     visit "/"
+#     expect(page).to have_text"Hello Battle!"
+#   end
+# end
+
+feature 'Filling player names' do
+  scenario 'User enters names at start' do
     visit "/"
-    expect(page).to have_text("Hello Battle!")
+    fill_in 'Player1_name', with: 'Gabe'
+    fill_in 'Player2_name', with: 'Carol'
+    click_button 'submit'
+    expect(page).to have_text("Gabe vs Carol")
   end
 end
